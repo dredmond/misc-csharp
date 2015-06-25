bce2f5f5f5d6accb34778cf1a9c4a9f380c35e9f
Donny Redmond <dredmond@cloud816.com>
2015-06-25 15:53:05 -0500

    Added a thread to run clicks from and set the sleep time to 100 milliseconds.

--- AutoClicker/AutoClicker/Form1.cs
+++ AutoClicker/AutoClicker/Form1.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -20,6 +21,8 @@ namespace AutoClicker
         private const int WM_LBUTTONUP = 0x0202;
         private const int WM_LBUTTON = 0x1;
         private bool _shown;
+        private Thread _thread;
+        private bool _running;
 
         public Form1()
         {
@@ -28,63 +31,83 @@ namespace AutoClicker
             Shown += Form1_Shown;
         }
 
+        private void ThreadProc()
+        {
+            WriteToLog("Click Thread Started!");
+
+            while (_running)
+            {
+                var p = Process.GetProcessesByName("Clicker Heroes").FirstOrDefault();
+
+                if (p == null)
+                {
+                    WriteToLog("Process not found!");
+                    return;
+                }
+
+                var x = 800;
+                var y = 450;
+                var lParam = (y << 16) | (x & 0xFFFF);
+
+                PostMessage(p.MainWindowHandle, WM_LBUTTONDOWN, WM_LBUTTON, lParam);
+                PostMessage(p.MainWindowHandle, WM_LBUTTONUP, 0, lParam);
+
+                WriteToLog(string.Format("Sent Click To: {0} At x = {1} y = {2}", p.ProcessName, x, y));
+
+                Thread.Sleep(100);
+            }
+
+            WriteToLog("Click Thread Stopped!");
+        }
+
         void Form1_Shown(object sender, EventArgs e)
         {
             _shown = true;
         }
 
-        [DllImport("user32.dll")]
-        private static extern bool PostMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr SetCapture(IntPtr hwnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        private static extern bool ReleaseCapture();
 
         [DllImport("user32.dll")]
-        private static extern IntPtr SendMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
+        private static extern bool PostMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
 
         [DllImport("user32")]
         public static extern int SetCursorPos(int x, int y);
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var p = Process.GetProcessesByName("Clicker Heroes").FirstOrDefault();
-
-            if (p == null)
+            if (_running && _thread != null)
             {
-                WriteToLog("Process not found!");
+                _running = false;
+                _thread.Join();
+                _thread = null;
+                button1.Text = @"Start";
                 return;
             }
 
-            var x = 500;
-            var y = 500;
-            var lParam = (y << 16) | (x & 0xFFFF);
-            SendMessage(p.MainWindowHandle, WM_SETCURSOR, p.MainWindowHandle.ToInt32(), lParam);
-            var success = SendMessage(p.MainWindowHandle, WM_LBUTTONDOWN, WM_LBUTTON, lParam);
-            SendMessage(p.MainWindowHandle, WM_SETCURSOR, p.MainWindowHandle.ToInt32(), lParam);
-            success = SendMessage(p.MainWindowHandle, WM_LBUTTONUP, 0, lParam);
-            SendMessage(p.MainWindowHandle, WM_SETCURSOR, p.MainWindowHandle.ToInt32(), lParam);
-
-            WriteToLog(string.Format("Process Found: {0}\r\nExecutable: {1}\r\nSuccess: {2}", p.ProcessName, p.MainModule.FileName, success));
+            _running = true;
+            _thread = new Thread(ThreadProc);
+            _thread.Start();
+            button1.Text = @"Stop";
         }
 
+        private delegate void WriteLogDelegate(string msg);
         private void WriteToLog(string msg)
         {
+            if (InvokeRequired)
+            {
+                var d = new WriteLogDelegate(WriteToLog);
+                Invoke(d, msg);
+                return;
+            }
+
             if (!_shown)
                 return;
 
             txtLog.AppendText(string.Format("[{0}] {1}\r\n", DateTime.Now.ToString("G"), msg));
-        }
-
-        protected override void WndProc(ref Message m)
-        {
-
-
-            if (m.Msg == WM_LBUTTONDOWN)
-            {
-                var x = m.LParam.ToInt32() & 0xFFFF;
-                var y = (m.LParam.ToInt32() >> 16) & 0xFFFF;
-
-                Console.WriteLine(m.Msg.ToString("X4") + " " + x + " " + y);
-            }
-
-            base.WndProc(ref m);
         }
     }
 }
