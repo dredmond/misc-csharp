16b2d999492a66569d07a41e8041135973ccb952
Donny Redmond <dredmond@cloud816.com>
2015-06-25 16:36:58 -0500

    Added input boxes for Clicks Per Second, X, and Y. Also added detection for Clicker Heroes not running when attempting to start the AutoClicker and while the AutoClicker is running.

--- AutoClicker/AutoClicker/Form1.Designer.cs
+++ AutoClicker/AutoClicker/Form1.Designer.cs
@@ -30,11 +30,17 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.txtLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtClicksPerSecond = new System.Windows.Forms.TextBox();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(172, 135);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,24 +50,90 @@
             // 
             // txtLog
             // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.txtLog.BackColor = System.Drawing.SystemColors.Window;
-            this.txtLog.Location = new System.Drawing.Point(12, 174);
+            this.txtLog.Location = new System.Drawing.Point(12, 41);
             this.txtLog.Multiline = true;
             this.txtLog.Name = "txtLog";
             this.txtLog.ReadOnly = true;
             this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtLog.Size = new System.Drawing.Size(402, 136);
+            this.txtLog.Size = new System.Drawing.Size(402, 269);
             this.txtLog.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Clicks Per Second:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "X:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(361, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Y:";
+            // 
+            // txtClicksPerSecond
+            // 
+            this.txtClicksPerSecond.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtClicksPerSecond.Location = new System.Drawing.Point(256, 14);
+            this.txtClicksPerSecond.Name = "txtClicksPerSecond";
+            this.txtClicksPerSecond.Size = new System.Drawing.Size(36, 20);
+            this.txtClicksPerSecond.TabIndex = 5;
+            this.txtClicksPerSecond.Text = "10";
+            // 
+            // txtX
+            // 
+            this.txtX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtX.Location = new System.Drawing.Point(321, 14);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(33, 20);
+            this.txtX.TabIndex = 6;
+            this.txtX.Text = "850";
+            // 
+            // txtY
+            // 
+            this.txtY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtY.Location = new System.Drawing.Point(381, 14);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(33, 20);
+            this.txtY.TabIndex = 7;
+            this.txtY.Text = "425";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(426, 322);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.txtClicksPerSecond);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtLog);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Clicker Heroes - AutoClicker";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,6 +143,12 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtClicksPerSecond;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
     }
 }
 
--- AutoClicker/AutoClicker/Form1.cs
+++ AutoClicker/AutoClicker/Form1.cs
@@ -1,28 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AutoClicker
 {
     public partial class Form1 : Form
     {
-        private const int WM_SETCURSOR = 0x0020;
-        private const int WM_MOUSEMOVE = 0x0200;
         private const int WM_LBUTTONDOWN = 0x0201;
         private const int WM_LBUTTONUP = 0x0202;
         private const int WM_LBUTTON = 0x1;
         private bool _shown;
         private Thread _thread;
         private bool _running;
+        private int _x;
+        private int _y;
+        private int _clicksPerSecond;
 
         public Form1()
         {
@@ -45,16 +40,14 @@ namespace AutoClicker
                     return;
                 }
 
-                var x = 850;
-                var y = 425;
-                var lParam = (y << 16) | (x & 0xFFFF);
+                var lParam = (_y << 16) | (_x & 0xFFFF);
 
                 PostMessage(p.MainWindowHandle, WM_LBUTTONDOWN, WM_LBUTTON, lParam);
                 PostMessage(p.MainWindowHandle, WM_LBUTTONUP, 0, lParam);
 
-                WriteToLog(string.Format("Sent Click To: {0} At x = {1} y = {2}", p.ProcessName, x, y));
+                WriteToLog(string.Format("Sent Click To: {0} At x = {1} y = {2}", p.ProcessName, _x, _y));
 
-                Thread.Sleep(100);
+                Thread.Sleep(1000 / _clicksPerSecond);
             }
 
             WriteToLog("Click Thread Stopped!");
@@ -65,17 +58,8 @@ namespace AutoClicker
             _shown = true;
         }
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
-        private static extern IntPtr SetCapture(IntPtr hwnd);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
-        private static extern bool ReleaseCapture();
-
         [DllImport("user32.dll")]
         private static extern bool PostMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
-
-        [DllImport("user32")]
-        public static extern int SetCursorPos(int x, int y);
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -83,10 +67,30 @@ namespace AutoClicker
             {
                 _running = false;
                 _thread = null;
+                txtClicksPerSecond.ReadOnly = false;
+                txtX.ReadOnly = false;
+                txtY.ReadOnly = false;
                 button1.Text = @"Start";
                 return;
             }
 
+            var p = Process.GetProcessesByName("Clicker Heroes").FirstOrDefault();
+
+            if (p == null)
+            {
+                WriteToLog("Process not found!");
+                return;
+            }
+
+            txtClicksPerSecond.ReadOnly = true;
+            txtX.ReadOnly = true;
+            txtY.ReadOnly = true;
+
+            _clicksPerSecond = Convert.ToInt32(txtClicksPerSecond.Text);
+            _x = Convert.ToInt32(txtX.Text);
+            _y = Convert.ToInt32(txtY.Text);
+
+            txtLog.Clear();
             _running = true;
             _thread = new Thread(ThreadProc);
             _thread.Start();
@@ -106,7 +110,7 @@ namespace AutoClicker
             if (!_shown)
                 return;
 
-            txtLog.AppendText(string.Format("[{0}] {1}\r\n", DateTime.Now.ToString("G"), msg));
+            txtLog.AppendText(string.Format("[{0}] {1}\r\n", DateTime.Now.ToString("hh:mm:ss.ffff tt"), msg));
         }
     }
 }
